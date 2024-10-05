7a9f054c4ebb26ebc756da7efa98b278de10bc6b
Thomas <thomas@tle-moel.fritz.box>
2024-10-05 12:06:25 +0200

    raycasting done

--- drawing/drawing_rays.c
+++ drawing/drawing_rays.c
@@ -6,11 +6,21 @@
 /*   By: thomas <thomas@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/04 16:14:26 by thomas            #+#    #+#             */
-/*   Updated: 2024/10/04 22:33:05 by thomas           ###   ########.fr       */
+/*   Updated: 2024/10/05 12:05:51 by thomas           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../cub3D.h"
+
+float	calculate_distance(t_data *data, t_ray ray)
+{
+	float	px;
+	float	py;
+
+	px = data->player.x * PIXELS_PER_CELL;
+	py = data->player.y * PIXELS_PER_CELL;
+	return (sqrt((ray.x - px) * (ray.x - px) + (ray.y - py) * (ray.y - py)));
+}
 
 void	draw_rays(t_data *data)
 {
@@ -20,85 +30,160 @@ void	draw_rays(t_data *data)
 	float	py;
 	float	xo;
 	float	yo;
-	t_ray	ray;
+	t_ray	ray_h;
+	t_ray	ray_v;
 	float	cotan;
+	float	dist_h;
+	float	dist_v;
 
-	ray.x = 0;
-	ray.y = 0;
 	xo = 0;
 	yo = 0;
 	px = data->player.x * PIXELS_PER_CELL;
 	py = data->player.y * PIXELS_PER_CELL;
-	ray.angle = data->player.angle;
+	ray_h.x = px;
+	ray_h.y = py;
+	ray_h.angle = data->player.angle - (DR * 30);
+	if (ray_h.angle < 0)
+		ray_h.angle += (2 * PI);
+	if (ray_h.angle > 2 * PI)
+		ray_h.angle -= (2 * PI);
+	ray_v.angle = ray_h.angle;
 	r = 0;
-	cotan = 1 / tan(ray.angle);
-	printf("px : %f, py : %f\n", px, py);
-	while (r < 1)
+	while (r < 60)
 	{
+		cotan = 1 / tan(ray_h.angle);
+		printf("angle : %f  %f\n", ray_h.angle, ray_v.angle);
 		// ---Check Horizontal Lines---
+		
 		dof = 0;
-		if (ray.angle < PI) // Looking up
+		dist_h = 1000000;
+		if (ray_h.angle < PI) // Looking up
 		{
-			printf("less than pi\n");
-			ray.y = ((int)data->player.y * PIXELS_PER_CELL) - 0.0001;
-			ray.x = (ray.y - py) * -1 * cotan + px;
+			ray_h.y = ((int)data->player.y * PIXELS_PER_CELL) - 0.0001;
+			ray_h.x = (ray_h.y - py) * -1 * cotan + px;
 			yo = - PIXELS_PER_CELL;
 			xo = yo * (-1 * cotan); 
 		}
-		else if (ray.angle > PI) // Looking down
+		else if (ray_h.angle > PI) // Looking down
 		{
-			printf("more than pi\n");
-			ray.y = ((int)data->player.y * PIXELS_PER_CELL) + PIXELS_PER_CELL;
-			ray.x = (ray.y - py) * -1 * cotan + px;
+			ray_h.y = ((int)data->player.y * PIXELS_PER_CELL) + PIXELS_PER_CELL;
+			ray_h.x = (ray_h.y - py) * -1 * cotan + px;
 			yo = PIXELS_PER_CELL;
 			xo = yo * (-1 * cotan);
 		}
-		if (ray.angle == 0 || ray.angle == PI) // Looking straight left or right
+		if (ray_h.angle == 0 || ray_h.angle == PI) // Looking straight left or right
 		{
-			printf("pi or 0\n");
-			ray.x = px;
-			ray.y = py;
+			ray_h.x = px;
+			ray_h.y = py;
 			dof = 16;
 		}
-		printf("ray.angle : %f\n", ray.angle);
-		printf("xo : %f, yo : %f\n", xo, yo);
 		while (dof < 16)
 		{
-			if (ray.x < 0)
+			if (ray_h.x < 0)
 			{
 				dof = 16;
-				ray.x = 0;
+				ray_h.x = 0;
 			}
-			if (ray.y < 0)
+			if (ray_h.y < 0)
 			{
 				dof = 16;
-				ray.y = 0;
+				ray_h.y = 0;
 			}
-			if (ray.x >= MINIMAP_WIDTH)
+			if (ray_h.x >= MINIMAP_WIDTH)
 			{
 				dof = 16;
-				ray.x = MINIMAP_WIDTH - 1;
+				ray_h.x = MINIMAP_WIDTH - 1;
 			}
-			if (ray.y >= MINIMAP_HEIGHT)
+			if (ray_h.y >= MINIMAP_HEIGHT)
 			{
 				dof = 16;
-				ray.y = MINIMAP_HEIGHT - 1;
+				ray_h.y = MINIMAP_HEIGHT - 1;
 			}
-			printf("dof : %i\n", dof);
-			printf("ray.x : %f, ray.y : %f\n", ray.x, ray.y);
-			if (ray.x >= 0 && ray.y >= 0 && ray.x < MINIMAP_WIDTH && ray.y < MINIMAP_HEIGHT && data->env.map[(int)(ray.y * CELLS_PER_PIXEL)][(int)(ray.x * CELLS_PER_PIXEL)] == '1')
-			{
-				printf("found wall!\n");
+			if (data->env.map[(int)(ray_h.y * CELLS_PER_PIXEL)][(int)(ray_h.x * CELLS_PER_PIXEL)] == '1')
 				dof = 16;
-			}
 			else
 			{
-				ray.x += xo;
-				ray.y += yo;
+				ray_h.x += xo;
+				ray_h.y += yo;
 				dof++;
 			}
 		}
+		dist_h = calculate_distance(data, ray_h);
+
+		// ---Check Vertical Lines---
+		ray_v.x = px;
+		ray_v.y = py;
+		dof = 0;
+		dist_v = 1000000;
+		if (ray_v.angle < (PI / 2) || ray_v.angle > (3 * PI / 2)) // Looking right
+		{
+			ray_v.x = ((int)data->player.x * PIXELS_PER_CELL) + PIXELS_PER_CELL;
+			ray_v.y = (ray_v.x - px) * -1 * tan(ray_v.angle) + py;
+			xo = PIXELS_PER_CELL;
+			yo = xo * -1 * tan(ray_v.angle); 
+		}
+		else if (ray_v.angle > (PI / 2) && ray_v.angle < (3 * PI / 2)) // Looking left
+		{
+			ray_v.x = ((int)data->player.x * PIXELS_PER_CELL) - 0.0001;
+			ray_v.y = (ray_v.x - px) * -1 * tan(ray_v.angle) + py;
+			xo = - PIXELS_PER_CELL;
+			yo = xo * -1 * tan(ray_v.angle);
+		}
+		if (ray_v.angle == (PI / 2) || ray_v.angle == (3 * PI / 2)) // Looking up or down
+		{
+			ray_v.x = px;
+			ray_v.y = py;
+			dof = 16;
+		}
+		while (dof < 16)
+		{
+			if (ray_v.x < 0)
+			{
+				dof = 16;
+				ray_v.x = 0;
+			}
+			if (ray_v.y < 0)
+			{
+				dof = 16;
+				ray_v.y = 0;
+			}
+			if (ray_v.x >= MINIMAP_WIDTH)
+			{
+				dof = 16;
+				ray_v.x = MINIMAP_WIDTH - 1;
+			}
+			if (ray_v.y >= MINIMAP_HEIGHT)
+			{
+				dof = 16;
+				ray_v.y = MINIMAP_HEIGHT - 1;
+			}
+			if (data->env.map[(int)(ray_v.y * CELLS_PER_PIXEL)][(int)(ray_v.x * CELLS_PER_PIXEL)] == '1')
+				dof = 16;
+			else
+			{
+				ray_v.x += xo;
+				ray_v.y += yo;
+				dof++;
+			}
+		}
+		dist_v = calculate_distance(data, ray_v);
+		printf("distH : %f, distV : %f\n", dist_h, dist_v);
+		if (dist_v < dist_h)
+		{
+			printf("ray_v.x : %f, ray_v.y : %f\n", ray_v.x, ray_v.y);
+			draw_line(data, (int)ray_v.x, (int)ray_v.y, convert_color(0, 128, 0));
+		}
+		else
+		{
+			printf("ray_h.x : %f, ray_h.y : %f\n", ray_h.x, ray_h.y);
+			draw_line(data, (int)ray_h.x, (int)ray_h.y, convert_color(0, 128, 0));
+		}
 		r++;
+		ray_h.angle += DR;
+		if (ray_h.angle < 0)
+			ray_h.angle += (2 * PI);
+		if (ray_h.angle > 2 * PI)
+			ray_h.angle -= (2 * PI);
+		ray_v.angle = ray_h.angle;
 	}
-	draw_line(data, (int)ray.x, (int)ray.y, convert_color(0, 128, 0));
 }
