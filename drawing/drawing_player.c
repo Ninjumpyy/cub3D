f59d54f250e32bb44c739d892a7443ad52500c93
Raja Pandiperumal <rpandipe@c3r6s2.42luxembourg.lu>
2024-10-28 14:41:59 +0100

    norme fixes

--- drawing/drawing_player.c
+++ drawing/drawing_player.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   drawing_player.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tle-moel <tle-moel@student.42.fr>          +#+  +:+       +#+        */
+/*   By: rpandipe <rpandipe.student.42luxembourg    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/23 14:53:15 by rpandipe          #+#    #+#             */
-/*   Updated: 2024/10/28 14:16:36 by tle-moel         ###   ########.fr       */
+/*   Updated: 2024/10/28 14:41:12 by rpandipe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,39 +37,49 @@ void	draw_player(t_data *data)
 	}
 }
 
+void	draw_player_pixels(int xy[2], t_img *img, int pxy[2], t_data *data)
+{
+	char	*src_pixel;
+	char	*dst_pixel;
+	int		dst_x;
+	int		dst_y;
+	int		color;
+
+	src_pixel = img->addr + (xy[1] * img->line_size + xy[0] * \
+							(img->bits_per_pixel / 8));
+	color = *(unsigned int *)src_pixel;
+	dst_x = (int)(pxy[0] + xy[0]);
+	dst_y = (int)(pxy[1] + xy[1]);
+	if (dst_x >= 0 && dst_x < CUB_WIDTH && dst_y >= 0 && dst_y < CUB_HEIGHT)
+	{
+		if (((color >> 24) & 0xFF) == 0)
+		{
+			dst_pixel = data->cub.addr + ((int)(pxy[1] + xy[1]) *\
+						data->cub.line_size + (int)(pxy[0] + xy[0]) *\
+						(data->cub.bits_per_pixel / 8));
+			*(unsigned int *)dst_pixel = color;
+		}
+	}
+}
+
 void	draw_player_sprite(t_data *data, t_img *img)
 {
 	int		x;
 	int		y;
-	char	*src_pixel;
-	char	*dst_pixel;
 	float	px;
 	float	py;
-	int		color;
 
 	y = 0;
 	px = (CUB_WIDTH / 2) - (img->width / 2);
 	py = (CUB_HEIGHT) - (img->height);
 	if (px < 0 || px >= CUB_WIDTH || py < 0 || py >= CUB_HEIGHT)
-		return;
+		return ;
 	while (y < img->height)
 	{
 		x = 0;
 		while (x < img->width)
 		{
-			src_pixel = img->addr + (y * img->line_size + x * (img->bits_per_pixel / 8));
-			color = *(unsigned int *)src_pixel;
-			int dst_x = (int)(px + x);
-            int dst_y = (int)(py + y);
-
-            if (dst_x >= 0 && dst_x < CUB_WIDTH && dst_y >= 0 && dst_y < CUB_HEIGHT)
-			{
-				if (((color >> 24) & 0xFF) == 0)
-				{
-					dst_pixel = data->cub.addr + ((int)(py + y) * data->cub.line_size + (int)(px + x) * (data->cub.bits_per_pixel / 8));
-					*(unsigned int *)dst_pixel = color;
-				}
-			}
+			draw_player_pixels((int[2]){x, y}, img, (int[2]){px, py}, data);
 			x++;
 		}
 		y++;
